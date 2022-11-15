82f862a3f6a2b3892444782ac3b3a3ebf9ac22df
Artyom Davydov <davart01@gmail.com>
2022-11-15 01:16:18 +0000

    add benchmark framework and first benchmarks

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 rm -rf external/
-rm perf/benchmark_custom_candidates/concurrent_hash_map.hpp
 
 wget https://github.com/boostorg/sync/archive/master.tar.gz -P external/boost/sync
-wget https://raw.githubusercontent.com/BlazingPhoenix/concurrent-hash-map/master/concurrent_hash_map/concurrent_hash_map.hpp -P perf/benchmark_custom_candidates/cuckoo
 
 tar -xzvf external/boost/sync/master.tar.gz --strip-components 1 -C external/boost/sync/
 rm external/boost/sync/master.tar.gz
---
+++ perf/benchmark_custom_candidates/stl/concurrent_stl_hash_map.hpp
@@ -0,0 +1,33 @@
+#include "oneapi/tbb/spin_mutex.h"
+#include <cstdint>
+#include <unordered_map>
+#include <mutex>
+#include <vector>
+#include <utility>
+
+// SHARDS NOW VALID ONLY FOR UINT32_T
+
+constexpr uint32_t shards_pow = 15;
+
+template<typename Key, typename Value>
+class concurrent_stl_hash_map {
+    std::array<std::pair<std::unordered_map<Key, Value>, tbb::spin_mutex>, 1 << shards_pow> shards;
+public:
+    decltype(auto) insert(const Key& key, const Value& value) {
+        auto& [map, mutex] = shards[key >> (32 - shards_pow)];
+        std::lock_guard<tbb::spin_mutex> lock(mutex);
+        return map.insert(std::make_pair(key, value));
+    }
+
+    decltype(auto) find(const Key& key) {
+        auto& [map, mutex] = shards[key >> (32 - shards_pow)];
+        std::lock_guard<tbb::spin_mutex> lock(mutex);
+        return map.count(key);
+    }
+
+    decltype(auto) erase(const Key& key) {
+        auto& [map, mutex] = shards[key >> (32 - shards_pow)];
+        std::lock_guard<tbb::spin_mutex> lock(mutex);
+        return map.erase(key);
+    }
+};
